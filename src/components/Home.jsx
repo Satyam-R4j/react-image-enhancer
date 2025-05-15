ba4edab97ad4dbb3541f9b114517949299187947
Satyam-R4j <satyamraj418@gmail.com>
2025-05-15 20:54:18 +0530

    Working on Image upload

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import Preview from "./Preview";
 import Upload from "./Upload";
 
-
-
 const Home = () => {
-  return <div className="">
-    <Upload/>
-    <Preview/>
-  </div>;
+  const [uploadImage, setUploadImage] = useState(null);
+  const [enhancedImage, setEnhancedImage] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const UploadImageHandler = (file) =>{
+    console.log(file);
+    
+  }
+
+  return (
+    <>
+      <Upload  UploadImageHandler={UploadImageHandler}/>
+      <Preview
+        loaded={loading}
+        uploaded={uploadImage}
+        enhanced={enhancedImage}
+      />
+    </>
+  );
 };
 
 export default Home;
--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -1,16 +1,30 @@
 import React from "react";
 
-const Upload = () => {
+const Upload = (props) => {
+  const ShowImageHandler = (e) => {
+    const file = e.target.files[0];
+
+    if (file) {
+      props.UploadImageHandler(file);
+    }
+  };
+
   return (
-    <div className="bg-lime-500 shadow-lg text-lg font-semibold rounded-2xl p-3 w-full max-w-2xl">
+    <div className="bg-lime-500 shadow-lg text-lg font-semibold rounded-2xl p-3 w-[25%] max-w-2xl">
       <label
         htmlFor="fileInput"
         className="block w-full cursor-pointer border-2 border-dashed px-2 py-1 border-gray-300 rounded-lg text-center hover:border-blue-400 transition-all "
       >
-        <input  type="file" className="hidden" id="fileInput"/>
+        <input
+          type="file"
+          className="hidden"
+          id="fileInput"
+          onChange={ShowImageHandler}
+        />
 
-      <span className="text-lg font-medium text-gray-600 ">Click and drag to upload your image</span>
-
+        <span className="text-lg font-medium text-gray-600 ">
+          Click and drag to upload your image
+        </span>
       </label>
     </div>
   );
